50bf2a5af59e57a707865753efef0ff5051105c1
Petko D. Petkov <pdp@websecurify.com>
2017-06-26 22:11:45 +0100

    Use ws-level-filesystem instead.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var leveljs = require('level-js');
 var levelup = require('levelup');
-var fs = require('level-filesystem');
+var fs = require('ws-level-filesystem');
 
 var db = levelup('level-filesystem', {db:leveljs});
 module.exports = fs(db);
