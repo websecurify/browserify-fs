ccda3a7cbd9a0a3d6865517359b189813e954c56
Petko D. Petkov <pdp@gnucitizen.org>
2018-07-09 22:26:59 +0100

    Added new module.

---
+++ fs.js
@@ -0,0 +1,6 @@
+var leveljs = require('level-js');
+var levelup = require('levelup');
+var fs = require('ws-level-filesystem');
+
+var db = levelup('level-filesystem', {db:leveljs});
+module.exports = fs(db);
--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-var leveljs = require('level-js');
-var levelup = require('levelup');
-var fs = require('ws-level-filesystem');
-
-var db = levelup('level-filesystem', {db:leveljs});
-module.exports = fs(db);
+module.exports = require('./fs');
